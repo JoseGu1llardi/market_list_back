25a6f15ceaff831e8c2f3175d16a3eb6f4a20649
Gui1lard1 <junior11_junior@hotmail.com>
2025-07-19 14:33:32 +0100

    Project settings

---
+++ src/main/java/com/guillard/appmarketlist/AppmarketlistApplication.java
@@ -0,0 +1,13 @@
+package com.guillard.appmarketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmarketlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmarketlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guillard/appmarketlist/AppmarketlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillard.appmarketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmarketlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
