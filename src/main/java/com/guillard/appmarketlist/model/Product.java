25cd40c378f53f03f6dcdfe48266d7fb9eab7ac0
Gui1lard1 <junior11_junior@hotmail.com>
2025-07-19 14:32:40 +0100

    Product model created

---
+++ src/main/java/com/guillard/appmarketlist/model/Product.java
@@ -0,0 +1,32 @@
+package com.guillard.appmarketlist.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_product")
+    private Integer id;
+
+    @Column(name = "product_name", length = 45, nullable = false)
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
